a5564eaae5088708cfa4008eec5255a8d93ca4f9
nokked <52399814+nokked@users.noreply.github.com>
2021-02-27 15:32:46 +0100

    trained the model

--- facerecognition.py
+++ facerecognition.py
@@ -1,14 +1,20 @@
 import cv2
 import sys
+import tensorflow as tf
+from tensorflow import keras
+import copy
+from keras.models import load_model
+from keras.optimizers import Adam
 
-from keras_vggface.vggface import VGGFace
-from keras_vggface.utils import decode_predictions
 
 import numpy as np
 
 def video_capture():
+    opt = Adam(lr=0.0001)
+    model = load_model('model_weights.h5')
+    model.summary()
 
-    model = VGGFace(model='resnet50')
+
     print('Inputs: %s' % model.inputs)
     print('Outputs: %s' % model.outputs)
 
@@ -28,15 +34,48 @@ def video_capture():
         # Extracting Frames
         check, frame = video.read()
 
-        # Scalefactor diminshes the image by a given factor, (10% in this case)
-        faces = face_cascade.detectMultiScale(frame, scaleFactor = 1.1,
-        minNeighbors = 5)
 
-        # x,y represents the upper left cornet of the face
+        img = copy.deepcopy(frame)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Scalefactor diminshes the image by a given factor, (10% in this case)
+        faces = face_cascade.detectMultiScale(frame, scaleFactor = 1.1, minNeighbors = 5)
+        faces_gray = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        # x,y represents the upper left cornet of the fa
         # w,h represents the widht and height
         # we create a rectangle with the rectangle function to frame the face
         for x,y,w,h in faces:
             frame_face = cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3);
+
+
+        for (x,y,w,h) in faces_gray:
+            fc = gray[y:y+h, x:x+w]
+
+            roi = cv2.resize(fc, (56,56))
+            pred = model.predict(roi[np.newaxis, :, :, np.newaxis])
+            print(pred)
+            text_idx=np.argmax(pred)
+
+            text_list = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
+            if text_idx == 0:
+                text= text_list[0]
+            if text_idx == 1:
+                text= text_list[1]
+            elif text_idx == 2:
+                text= text_list[2]
+            elif text_idx == 3:
+                text= text_list[3]
+            elif text_idx == 4:
+                text= text_list[4]
+            elif text_idx == 5:
+                text= text_list[5]
+            elif text_idx == 6:
+                text= text_list[6]
+            cv2.putText(frame, text, (x, y-5),
+               cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 0, 255), 2)
+            img = cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 2)
+
+
         try:
             x = faces[0][0]
             y = faces[0][1]
@@ -47,30 +86,8 @@ def video_capture():
             y = 1
             w = 1
             h = 1
-        cropped = frame[y:y+h, x:x+w]
 
-
-        width = 224
-        height = 224
-        dim = (width, height)
-
-        cropped_resized = cv2.resize(cropped, dim, interpolation = cv2.INTER_AREA)
-
-        cropped_resized = np.expand_dims(cropped_resized, axis=0)
-
-        yhat = model.predict(cropped_resized)
-
-        # convert prediction into names
-        results = decode_predictions(yhat)
-        # display most likely results
-        for result in results[0]:
-        	print('%s: %.3f%%' % (result[0], result[1]*100))
-
-        #except:
-        #    pass
-        #cropped = faces[[0]:[1],[2]:[3]]
-
-        # display the imageq
+        # display the image
         cv2.imshow('Face Detector', frame_face)
 
         # To continue to display the image, we need to use waitKey
