16e8b1aede822b364c589ebeb8106e7c7803dea3
nokked <52399814+nokked@users.noreply.github.com>
2021-03-02 21:35:06 +0100

    Updated a bug in the facerecognition.py

--- facerecognition.py
+++ facerecognition.py
@@ -2,18 +2,19 @@ import cv2
 import sys
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras.models import model_from_json
 import copy
 from keras.models import load_model
 from keras.optimizers import Adam
+import time
 
 
 import numpy as np
 
-def video_capture():
-    opt = Adam(lr=0.0001)
-    model = load_model('model_weights.h5')
-    model.summary()
+model = load_model(r"C:\Users\berna\Documents\GitHub\Emotiondetection/")
+model.load_weights(r"C:\Users\berna\Documents\GitHub\Emotiondetection/model_weights.h5")
 
+def video_capture():
 
     print('Inputs: %s' % model.inputs)
     print('Outputs: %s' % model.outputs)
@@ -23,7 +24,7 @@ def video_capture():
     # The cascade is an XML file that contins the data to detect faces.
     # Haar Cascade is a classifier that is used to identify specific objects
     # in this case it is for frontal faces
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
     # We use a function called VideoCapture to capture the webcam's video feed
     #video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -33,46 +34,31 @@ def video_capture():
     while True:
         # Extracting Frames
         check, frame = video.read()
-
-
         img = copy.deepcopy(frame)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         # Scalefactor diminshes the image by a given factor, (10% in this case)
         faces = face_cascade.detectMultiScale(frame, scaleFactor = 1.1, minNeighbors = 5)
-        faces_gray = face_cascade.detectMultiScale(gray, 1.3, 5)
 
         # x,y represents the upper left cornet of the fa
         # w,h represents the widht and height
         # we create a rectangle with the rectangle function to frame the face
+
         for x,y,w,h in faces:
             frame_face = cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 3);
+            #faces_gray = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-
-        for (x,y,w,h) in faces_gray:
+            gray = cv2.cvtColor(frame_face, cv2.COLOR_BGR2GRAY)
             fc = gray[y:y+h, x:x+w]
 
             roi = cv2.resize(fc, (56,56))
             pred = model.predict(roi[np.newaxis, :, :, np.newaxis])
-            print(pred)
             text_idx=np.argmax(pred)
-
             text_list = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
-            if text_idx == 0:
-                text= text_list[0]
-            if text_idx == 1:
-                text= text_list[1]
-            elif text_idx == 2:
-                text= text_list[2]
-            elif text_idx == 3:
-                text= text_list[3]
-            elif text_idx == 4:
-                text= text_list[4]
-            elif text_idx == 5:
-                text= text_list[5]
-            elif text_idx == 6:
-                text= text_list[6]
+            text = text_list[text_idx]
+            print(text)
+
             cv2.putText(frame, text, (x, y-5),
-               cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 0, 255), 2)
+            cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 0, 255), 2)
             img = cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 2)
 
 
